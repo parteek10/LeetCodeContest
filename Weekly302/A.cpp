f9bd7201c8ab429454dce272e7ca52e834bbd5e8
Mayank <82897488+mayank3005@users.noreply.github.com>
2022-10-19 18:02:30 +0530

    Create A.cpp

---
+++ Weekly302/A.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> numberOfPairs(vector<int>& nums) {
+        
+        map<int,int> mp;
+        for(auto i:nums) mp[i]++;
+        
+        vector<int> ans(2,0);
+        for(auto i:mp){
+            ans[0]+=i.second/2;
+        }
+        for(auto i:mp){
+            ans[1]+=i.second%2;
+        }
+        return ans;
+    }
+};
