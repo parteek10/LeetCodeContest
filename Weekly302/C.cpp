26f5ff5a0b1f6485d559b9636052e091a266ec0e
Mayank <82897488+mayank3005@users.noreply.github.com>
2022-10-19 18:06:53 +0530

    Create C.cpp

---
+++ Weekly302/C.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
+        
+        vector<int> ans;
+        
+        int n=nums.size();
+        
+        for(int c=0;c<queries.size();c++){
+            int k=queries[c][0],trim=queries[c][1];
+            priority_queue<pair<string,int>> pq;
+            for(int i=0;i<n;i++){
+                int x=nums[i].size();
+                pq.push({nums[i].substr(x-trim,trim),i});
+                if(pq.size()>k) pq.pop();
+            }
+            ans.push_back(pq.top().second);
+        }
+        
+        return ans;
+    }
+};
