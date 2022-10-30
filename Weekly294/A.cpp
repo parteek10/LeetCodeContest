19937a5aca2d63a8c73ee13833bb860b5fff69d4
ankitk84 <79309319+ankitk84@users.noreply.github.com>
2022-10-30 21:25:42 +0530

    Create A.cpp
    This file contains the code for problem A from Contest Weekly294

---
+++ Weekly294/A.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/percentage-of-letter-in-string/
+
+// code ::
+
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+
+    int len = s.length(), count = 0;
+
+        for(int i = 0; i < len; i++)
+        {
+            if(s[i] == letter)
+            {
+                count++;
+            }
+        }
+        int per = (count*100)/len;
+
+        return per;
+    }
+};
